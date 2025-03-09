91921cd726d8543d2876b53a25b802afa4ae5ba1
SkyNetCloud <mcboxer1201@gmail.com>
2025-03-09 01:51:15 -0500

    Initial commit

---
+++ src/main/java/ca/skynetcloud/cobblegamba/Cobblegamba.java
@@ -0,0 +1,10 @@
+package ca.skynetcloud.cobblegamba;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cobblegamba implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
